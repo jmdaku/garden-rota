3b4223c7eac3c4092e03ae4a13bdfebf56e821df
jmdaku <jmdaku@gmail.com>
2024-04-05 12:56:35 -0400

    added files from Spring Boot initialization

---
+++ src/main/java/com/garden/gardenrotation/GardenRotationApplication.java
@@ -0,0 +1,13 @@
+package com.garden.gardenrotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenRotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenRotationApplication.class, args);
+	}
+
+}
