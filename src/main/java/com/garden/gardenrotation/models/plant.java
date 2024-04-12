f6218266e6d640ba11fabe7cf0f063392c2b8950
jmdaku <jmdaku@gmail.com>
2024-04-12 17:54:31 -0400

    added plant repository

--- src/main/java/com/garden/gardenrotation/models/plant.java
+++ src/main/java/com/garden/gardenrotation/models/plant.java
@@ -2,10 +2,12 @@ package com.garden.gardenrotation.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class plant {
 
+    @Id
     @GeneratedValue
     private int id;
 
