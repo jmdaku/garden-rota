b374e9af8440bf52ae54aacdccda81377da3fbda
jmdaku <jmdaku@gmail.com>
2024-04-26 15:24:33 -0400

    added basic pages

---
+++ frontend/garden-rota-front/src/components/Plants.jsx
@@ -0,0 +1,9 @@
+import '../App.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Plants() {
+    return(
+        <div>Plants</div>
+    );
+}
--- frontend/garden-rota-front/src/main.jsx
+++ frontend/garden-rota-front/src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+reportWebVitals();
