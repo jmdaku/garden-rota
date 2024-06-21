7c96c46bde0763ecba1fea2c222f9623aafa6210
jmdaku <jmdaku@gmail.com>
2024-06-21 15:25:10 -0400

    troubleshooting Vite

--- frontend/garden-rota-front/src/components/Families.jsx
+++
@@ -1,9 +0,0 @@
-import '../App.css';
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-export default function Families() {
-    return(
-        <div>Families</div>
-    );
-}
